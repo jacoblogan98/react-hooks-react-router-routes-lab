ef40dd591c62773a55e68f75855b962345513d3a
Jacob Logan <jacoblogan98@gmail.com>
2022-04-16 12:37:20 -0400

    passed tests

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieCards = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        <h2>Title: {movie.title}</h2>
+        <h2>Time: {movie.time}</h2>
+        <ul>
+          {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <h1>Movies Page</h1>
+      {movieCards}
+    </>
+  )
 }
 
 export default Movies;
